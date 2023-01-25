e76e9a5ea49b0633ad74a8fa583d567fea8be96a
nianny <xiaonianhe@hotmail.com>
2023-01-25 23:14:48 +0800

    yay???

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,81 +1,99 @@
-const page_width = 10;
-
+const page_width = 50;
+const page_start = 100;
 let answer = "Hallo halloooo halloooooo?";
-let counter = 0;
-let answer_arr = [];
 
 let current = "";
 let backspace = false;
 let backspace_time = 0;
-let current_time = 0;
-
-function parse_string(){
-    let temp = answer.split(" ");
-
-    let line = 0;
-    let total_length = 0;
-    answer_arr[line] = "";
-    for (let i=0; i<temp.length; i++) {
-        if (temp[i].length + total_length > page_width) { //REMEMBER TO CHECK THAT LENGTHS OF ALL WORDS LESS THAN PAGE_WIDTH
-            line ++;
-            total_length = 0;
-            answer_arr[line]=(temp[i]+" ");
-        }
-        else{
-            answer_arr[line] += (temp[i] + " ");
-        }
-    }
-
-    for (let i=0; i<answer_arr.length; i++) {
-        answer_arr[i] = answer_arr[i].split(" ");
-    }
-}
+let frame = 0;
 function setup() {
     createCanvas(400, 400);
     
 
     // answer_arr = answer.split(" ");
-    parse_string();
-    console.log(answer_arr);
 }
 
 function displayText(){
-    let words_displayed = 0;
-    let character_displayed = 0;
-    console.log(answer_arr);
-    for (let i=0; i<answer_arr.length; i++) {
-        character_displayed = 0;
-        for (let j=0; j<answer_arr[i].length; j++){
-            // console.log(words_displayed, counter);
-            if (words_displayed < counter){
-                fill(200);
-                text(answer_arr[i][j]+" ", 100+6*character_displayed, 100+i*50);
-                character_displayed+=answer_arr[i][j].length+1;
-                words_displayed++;
-            }
-            // else if (words_displaed == counter){
+    let ans_arr = answer.split(" ");
+    let cur_arr = current.split(" ");
+    console.log(ans_arr);
+    console.log(cur_arr);
 
-            // }  
-            else{
-                fill(100);
-                text(answer_arr[i][j]+" ", 100+6*character_displayed, 100+i*50);
-                character_displayed+=answer_arr[i][j].length+1;
-                words_displayed++;
+    if(cur_arr.length > ans_arr.length){
+        return;
+    }
+    let char_num = 0;
+    let line_num = 0;
+    for (let i=0; i<cur_arr.length; i++){
+        if (max(cur_arr[i].length, ans_arr[i].length) + char_num > 15){
+            char_num = 0;
+            line_num++;
+        }
+        if (cur_arr[i].length < ans_arr[i].length){
+            for (let j=0; j<cur_arr[i].length; j++){
+                if (cur_arr[i][j] == ans_arr[i][j]){
+                    fill(200);
+                    text(ans_arr[i][j], 100+char_num*6, 100+line_num*20);
+                    
+                }
+                else{
+                    fill(160, 49, 49);
+                    text(cur_arr[i][j], 100+char_num*6, 100+line_num*20);
+                }
+                char_num++;
+            }
+
+            for (let j=cur_arr[i].length; j<ans_arr[i].length; j++) {
+                fill(150);
+                text(ans_arr[i][j], 100+char_num*6, 100+line_num*20);
+                char_num++;
             }
         }
+        else{
+            for(let j=0; j<ans_arr[i].length; j++){
+                if (cur_arr[i][j] == ans_arr[i][j]){
+                    fill(200);
+                    text(ans_arr[i][j], 100+char_num*6, 100+line_num*20);
+                    
+                }
+                else{
+                    fill(200, 49, 49);
+                    text(cur_arr[i][j], 100+char_num*6, 100+line_num*20);
+                }
+                char_num++;
+            }
+
+            for(let j=ans_arr[i].length; j<cur_arr[i].length; j++){
+                fill(160, 30, 30);
+                text(cur_arr[i][j], 100+char_num*6, 100+line_num*20);
+                char_num++;
+            }
+        }
+        text(" ", 100+char_num*6, 100+line_num*20);
+        char_num++;
+    }
+
+    for (let i=cur_arr.length; i<ans_arr.length; i++){
+        if(ans_arr[i].length + char_num > 15){
+            char_num = 0;
+            line_num++;
+        }
+        fill(150);
+        text(ans_arr[i]+" ", 100+char_num*6, 100+line_num*20);
+        char_num+=(ans_arr[i].length+1);
     }
 }
 
 function draw() {
-    current_time++;
-    background(54);
+    frame++;
+    background(56);
     // background(220);
     // circle(100, 100, 100);
     textFont('Ubuntu Mono');
     fill(255);
     text(current, 0, 400);
     
-    if (current_time%3 == 0 && backspace && current_time > backspace_time){
+    if (frame%3 == 0 && backspace && frame > backspace_time){
         current=current.substring(0, current.length-1);
         onInputChange();
     }
@@ -86,13 +104,19 @@ function draw() {
 }
 
 function onInputChange(){
-    if (current == answer.split(" ")[counter]){
-        counter++;
-        current = ""; 
-    }
+    // if (current == answer.split(" ")[counter]){
+    //     counter++;
+    //     current = ""; 
+    // }
+    // if (current[current.length -1] == " "){
+
+    // }
 }
 
 function keyTyped(){
+    if (key == " "){
+        if (current[current.length -1] == " ") return;
+    }
     current += key;
     onInputChange();
 }
@@ -101,7 +125,7 @@ function keyPressed(){
     if (keyCode == BACKSPACE){
         current=current.substring(0, current.length-1);
 
-        backspace_time = current_time+20;
+        backspace_time = frame+20;
         backspace = true;
         onInputChange();
     }
